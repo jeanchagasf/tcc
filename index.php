b2af3014593cbb65a985bba57fcce7b71972f42f
jean.chagas <jean.chagas@kinghost.com.br>
2018-08-21 10:58:25 -0300

    Arquivos gerados pela IDE

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jean Chagas
+ * Date: 21/08/18
+ * Time: 10:50 *
+ * About: self::Framework
+ *
+ **/
+
+/**
+ *   Reportar todos erros do PHP
+ **/
+
+error_reporting(E_ALL);
+
+/**
+ * Load do Environment
+ **/
+
+require_once ('settings.php');
